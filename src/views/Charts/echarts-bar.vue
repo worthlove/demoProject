34cad06fcc6e562d33dad600e3ec8fdf5929c47c
GZF <13038085710g@gmail.com>
2023-09-03 19:51:32 +0800

    格式化更改

--- src/views/Charts/echarts-bar.vue
+++ src/views/Charts/echarts-bar.vue
@@ -5,54 +5,55 @@
 </template>
 
 <script>
-import _default from 'vuex'
 export default {
-  name: 'echartsBar',
-  data () {
+  name: "echartsBar",
+  data() {
     return {
-      myChart: null
-    }
+      myChart: null,
+    };
   },
-  mounted () {
+  mounted() {
     // 窗口适配问题解决
-    window.addEventListener('resize', () => {
-      this.resizeChart()
-    })
-    this.drawChart()
+    window.addEventListener("resize", () => {
+      this.resizeChart();
+    });
+    this.drawChart();
   },
   methods: {
-    drawChart () {
+    drawChart() {
       // 基于准备好的dom，初始化echarts实例  这个和上面的main对应
-      this.myChart = this.$echarts.init(document.getElementById('main'))
+      this.myChart = this.$echarts.init(document.getElementById("main"));
       // 指定图表的配置项和数据
       let option = {
         title: {
-          text: 'ECharts 柱状图'
+          text: "ECharts 柱状图",
         },
         tooltip: {},
         legend: {
-          data: ['销量']
+          data: ["销量"],
         },
         xAxis: {
-          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
         },
         yAxis: {},
         series: [
           {
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-          }
-        ]
-      }
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20],
+          },
+        ],
+      };
       // 使用刚指定的配置项和数据显示图表。
-      this.myChart.setOption(option)
+      this.myChart.setOption(option);
     },
-    resizeChart () {
-      this.myChart ? this.myChart.resize() : false
-    }
-  }
-}
+    // 使用一个方法，调整图表的大小
+    /* 如果 this.myChart 存在（即不为 null 或 undefined），则调用 this.myChart.resize() 方法来重新调整图表的大小。如果 this.myChart 不存在，三元表达式的结果为 false，意味着不进行任何操作。 */
+    resizeChart() {
+      this.myChart ? this.myChart.resize() : false;
+    },
+  },
+};
 </script>
 
 <style lang="stylus" scoped>
--- src/views/Charts/echarts-pie.vue
+++ src/views/Charts/echarts-pie.vue
@@ -2,84 +2,82 @@
   <div class="dork">
     <div id="pie" style="width: 100%; height: 100%"></div>
   </div>
-
 </template>
 
 <script>
-
 export default {
-  name: 'echarts-pie',
-  data () {
+  name: "echarts-pie",
+  data() {
     return {
-      pies: null
-    }
+      pies: null,
+    };
   },
-  mounted () {
-    window.addEventListener('resize', () => {
-      this.resizeChart()
-    })
-    this.drawChart()
+  mounted() {
+    window.addEventListener("resize", () => {
+      this.resizeChart();
+    });
+    this.drawChart();
   },
   methods: {
-    drawChart () {
-      this.pies = this.$echarts.init(document.getElementById('pie'))
+    drawChart() {
+      this.pies = this.$echarts.init(document.getElementById("pie"));
       let option = {
         title: {
-          text: 'Referer of a Website',
-          subtext: 'Fake Data',
-          left: 'center'
+          text: "Referer of a Website",
+          subtext: "Fake Data",
+          left: "center",
         },
         tooltip: {
-          trigger: 'item'
+          trigger: "item",
         },
         legend: {
-          orient: 'vertical',
-          left: 'left'
+          orient: "vertical",
+          left: "left",
         },
         series: [
           {
-            name: 'Access From',
-            type: 'pie',
-            radius: '50%',
+            name: "Access From",
+            type: "pie",
+            radius: "50%",
             data: [
               {
                 value: 1048,
-                name: 'Search Engine'
+                name: "Search Engine",
               },
               {
                 value: 735,
-                name: 'Direct'
+                name: "Direct",
               },
               {
                 value: 580,
-                name: 'Email'
+                name: "Email",
               },
               {
                 value: 484,
-                name: 'Union Ads'
+                name: "Union Ads",
               },
               {
                 value: 300,
-                name: 'Video Ads'
-              }
+                name: "Video Ads",
+              },
             ],
             emphasis: {
               itemStyle: {
                 shadowBlur: 10,
                 shadowOffsetX: 0,
-                shadowColor: 'rgba(0, 0, 0, 0.5)'
-              }
-            }
-          }
-        ]
-      }
-      this.pies.setOption(option)
+                shadowColor: "rgba(0, 0, 0, 0.5)",
+              },
+            },
+          },
+        ],
+      };
+      this.pies.setOption(option);
     },
-    resizeChart () {
-      this.pies ? this.pies.resize() : false
-    }
-  }
-}
+    resizeChart() {
+      this.pies ? this.pies.resize() : false;
+    },
+  },
+};
 </script>
 
 <style lang="stylus" scoped>
